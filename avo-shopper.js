04db7e93eb259c87bfd281b467b748dd2b51b797
v203 <vuyisamr203@gmail.com>
2021-11-23 19:14:07 +0200

    rev-VI

--- avo-shopper.js
+++ avo-shopper.js
@@ -1,49 +1,92 @@
-module.exports = function(pool) {
+module.exports = function (pool) {
 
 	async function createShop(shopName) {
-		const result = await pool.query(`insert into shop (name) values ($1) returning id`, [shopName]);
+		try {
+
+			const result = await pool.query(`insert into shop (name) values ($1) returning id`, [shopName]);
+		try {
+
+		} catch (error) {
+			console.log(`creatShop fn error ${error}`);
+		}
 		if (result.rowCount === 1) {
 			return result.rows[0].id;
 		}
 		return null;
+
+		} catch (error) {
+
+		}
+		
 	}
 
 	async function listShops() {
-		const result = await pool.query(`select * from shop`);
-		return result.rows;
+		try {
+			const result = await pool.query(`select * from shop`);
+			return result.rows;
+
+		} catch (error) {
+			console.log(`listShop fn error ${error}`);
+		}
+
 	}
 
 	async function dealsForShop(shopId) {
-		const result = await pool.query(`select * from avo_deal where shop_id = $1`, [shopId]);
-		console.log(result.rows);
-		return result.rows;
+		try {
+			const result = await pool.query(`select * from avo_deal where shop_id = $1`, [shopId]);
+			console.log(result.rows);
+			return result.rows;
+		}
+			
+		catch (error) {
+			console.log(`dealsForShop error ${error}`);
 	}
+} 
 
 	async function topFiveDeals() {
-		const bestPriceSQL = `select name as shop_name, price, qty, round((price/qty), 2) as unit_price from avo_deal 
+		try {
+			const bestPriceSQL = `select name as shop_name, price, qty, round((price/qty), 2) as unit_price from avo_deal 
 		join shop on shop.id = avo_deal.shop_id 
 		order by (price/qty) asc 
 		limit 5`
 
-		const result = await pool.query(bestPriceSQL);
-		return result.rows;
+			const result = await pool.query(bestPriceSQL);
+			return result.rows;
+
+		} catch (error) {
+			console.log(`top5 fn error ${error}`);
+
+		}
+
 
 	}
 
 	async function createDeal(shopId, qty, price) {
-		await pool.query(`insert into avo_deal (shop_id, qty, price) values ($1, $2, $3)`, 
-			[shopId, qty, price]);
+		try {
+			await pool.query(`insert into avo_deal (shop_id, qty, price) values ($1, $2, $3)`,
+				[shopId, qty, price]);
+
+		} catch (error) {
+			console.log(`creatDeal fn error ${error}`);
+		}
+
 	}
 
 	async function recommendDeals(amount) {
-		const result = await pool.query(`
+		try {
+			const result = await pool.query(`
 			select name, price, qty, round((price/qty), 2) as unit_price from avo_deal 
 			join shop on shop.id = avo_deal.shop_id 
 			where price <= $1 order by unit_price asc`, [amount]);
 
-		return result.rows;
-	}
+			return result.rows;
 
+
+		} catch (error) {
+			console.log(`recommend fn error ${error}`);
+		}
+
+	}
 	return {
 		createDeal,
 		createShop,
